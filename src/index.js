0e3fe40031166dfd9252a7afcb9497988b44ca89
pradityamf <pradityamf@gmail.com>
2022-01-26 09:39:31 +0700

    jsx

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// const name = "Faiz"
+// const element = <h1>{sayWelcome(name)}</h1>
+
+// function sayWelcome(name) {
+//   return "Welcome " + name
+// }
+
+// ReactDOM.render(element, document.getElementById('example'))
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
